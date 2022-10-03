106ca6c6a5e83dae57c7ca5da4fd5194bd81704c
Daniel Konecny <dkonecny97@gmail.com>
2022-10-03 23:26:59 +0200

    Reformat files for better readability

--- crnn/eval.py
+++ crnn/eval.py
@@ -9,10 +9,8 @@ from metrics import SequenceAccuracy, EditDistance
 from models import build_model
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, required=True, 
-                    help='The config file path.')
-parser.add_argument('--weight', type=str, required=True, 
-                    help='The saved weight path.')
+parser.add_argument('--config', type=str, required=True, help='The config file path.')
+parser.add_argument('--weight', type=str, required=True, help='The saved weight path.')
 args = parser.parse_args()
 
 with open(args.config) as f:
@@ -21,8 +19,10 @@ pprint.pprint(config)
 
 dataset_builder = DatasetBuilder(**config['dataset_builder'])
 ds = dataset_builder(config['ann_paths'], config['batch_size'], False)
-model = build_model(dataset_builder.num_classes,
-                    weight=args.weight,
-                    img_shape=config['dataset_builder']['img_shape'])
+model = build_model(
+    dataset_builder.num_classes,
+    weight=args.weight,
+    img_shape=config['dataset_builder']['img_shape']
+)
 model.compile(loss=CTCLoss(), metrics=[SequenceAccuracy(), EditDistance()])
 model.evaluate(ds)
--- crnn/export.py
+++ crnn/export.py
@@ -8,14 +8,11 @@ from models import build_model
 from decoders import CTCGreedyDecoder, CTCBeamSearchDecoder
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=Path, required=True, 
-                    help='The config file path.')
-parser.add_argument('--weight', type=str, required=True, default='',
-                    help='The saved weight path.')
+parser.add_argument('--config', type=Path, required=True, help='The config file path.')
+parser.add_argument('--weight', type=str, required=True, default='', help='The saved weight path.')
 parser.add_argument('--pre', type=str, help='pre processing.')
 parser.add_argument('--post', type=str, help='Post processing.')
-parser.add_argument('--output', type=str, required=True, 
-                    help='The output path.')
+parser.add_argument('--output', type=str, required=True, help='The output path.')
 args = parser.parse_args()
 
 with args.config.open() as f:
@@ -25,7 +22,7 @@ with open(config['table_path']) as f:
     num_classes = len(f.readlines())
 
 if args.pre == 'rescale':
-    preprocess = keras.layers.experimental.preprocessing.Rescaling(1./255)
+    preprocess = keras.layers.experimental.preprocessing.Rescaling(1. / 255)
 else:
     preprocess = None
 
@@ -38,10 +35,12 @@ elif args.post == 'beam_search':
 else:
     postprocess = None
 
-model = build_model(num_classes, 
-                    weight=args.weight,
-                    preprocess=preprocess,
-                    postprocess=postprocess,
-                    img_shape=config['img_shape'])
+model = build_model(
+    num_classes,
+    weight=args.weight,
+    preprocess=preprocess,
+    postprocess=postprocess,
+    img_shape=config['img_shape']
+)
 model.summary()
 model.save(args.output)
